ee645b3d8c29655296072c3374a73c068916fd66
Harry Solovay <harrysolovay@gmail.com>
2020-01-26 09:23:26 -0800

    chore: increment version before release

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,8 @@
-const tsTranspiledEvent = new Event('tsTranspiled');
-const workerFile = window.URL.createObjectURL(new Blob([`
+const tsTranspiledEvent = new Event('tsTranspiled')
+const workerFile = window.URL.createObjectURL(
+  new Blob(
+    [
+      `
         importScripts('https://unpkg.com/typescript@latest')
 
         const load = sourceUrl => {
@@ -17,38 +20,45 @@ const workerFile = window.URL.createObjectURL(new Blob([`
           return xhr.status == 200 ? xhr.responseText : ''
         }
 
-        onmessage = ({data: sourceUrl}) => {
-          const raw = load(sourceUrl)
+        onmessage = ({data: [sourceUrl, sourceCode]}) => {
+          const raw = sourceCode ? sourceCode : load(sourceUrl)
           const transpiled = ts.transpile(raw)
           postMessage(transpiled)
         }
-      `], {
-  type: 'text/javascript'
-}));
+      `,
+    ],
+    {
+      type: 'text/javascript',
+    },
+  ),
+)
 window.addEventListener('DOMContentLoaded', async () => {
-  const scripts = document.getElementsByTagName('script');
-  let pending = [];
+  const scripts = document.getElementsByTagName('script')
+  let pending = []
 
   for (let i = 0; i < scripts.length; i++) {
     if (scripts[i].type === 'text/typescript') {
-      const src = scripts[i].src;
-      pending.push(new Promise(resolve => {
-        const w = new Worker(workerFile);
-        w.postMessage(src);
+      const src = scripts[i].src
+      const innerHtml = src ? null : scripts[i].innerHTML
+      pending.push(
+        new Promise(resolve => {
+          const w = new Worker(workerFile)
+          w.postMessage([src, innerHtml])
 
-        w.onmessage = (_ref) => {
-          let transpiled = _ref.data;
-          const newScript = document.createElement('script');
-          newScript.innerHTML = `window.addEventListener('tsTranspiled', function() {
+          w.onmessage = _ref => {
+            let transpiled = _ref.data
+            const newScript = document.createElement('script')
+            newScript.innerHTML = `window.addEventListener('tsTranspiled', function() {
               ${transpiled}
-            })`;
-          scripts[i].replaceWith(newScript);
-          resolve();
-        };
-      }));
+            })`
+            scripts[i].replaceWith(newScript)
+            resolve()
+          }
+        }),
+      )
     }
   }
 
-  await Promise.all(pending);
-  window.dispatchEvent(tsTranspiledEvent);
-});
+  await Promise.all(pending)
+  window.dispatchEvent(tsTranspiledEvent)
+})
